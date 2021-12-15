1fb6ac6fb472cbef192119df46604795434b3a07
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-15 11:57:58 -0600

    .

---
+++ Functional Programming/slice pure function.js
@@ -0,0 +1,13 @@
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+    let newArray = []
+    for (i = beginSlice; i < endSlice; i++){
+        newArray.push(anim[i])
+    }
+    return newArray
+    // Only change code above this line
+  }
+  
+  const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+  console.log(sliceArray(inputAnim, 1, 3))
+  console.log(inputAnim)
