93e13764a3f287e3966cbc26118540f4b56c0b3d
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-19 07:48:48 -0600

    done

--- Intermediate Algo/Wherefor art thou.js
+++ Intermediate Algo/Wherefor art thou.js
@@ -1,10 +1,18 @@
 function whatIsInAName(collection, source) {
-    const arr = [];
+    let arr = [collection];
+    arr.map(item => item.last == 'Capulet')
     // Only change code below this line
+
   
   
     // Only change code above this line
     return arr;
   }
   
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+  console.log(JSON.stringify(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" })))
+
+  /*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.
+  */
