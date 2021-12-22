67334574c332afb6aacccfea98cea5823eb78164
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-21 22:53:06 -0600

    inital commit

---
+++ Intermediate Algo/Sorted Union.js
@@ -0,0 +1,7 @@
+function uniteUnique(arr) {
+    return arr;
+  }
+  
+  console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]))
+  console.log(uniteUnique([1, 2, 3], [5, 2, 1]))
+  console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8]))
