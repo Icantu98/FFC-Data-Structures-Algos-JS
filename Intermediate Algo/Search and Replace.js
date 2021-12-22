15e6aa40dd4101535446acf3e6a0f139a8e50ef8
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-21 19:04:28 -0600

    missing 1 test case

--- Intermediate Algo/Search and Replace.js
+++ Intermediate Algo/Search and Replace.js
@@ -1,5 +1,37 @@
 function myReplace(str, before, after) {
-    return str;
+    // usefule variables
+    let result = []
+    let index  // index of hunted word
+    str = str.split(' ') // split str to find index for specific word
+    // searching for specified word with for loop
+    for (let i = 0; i < str.length; i++){
+        if (str[i] == before){
+            if(str[i][0] == str[i][0].toUpperCase()){
+                index = after.charAt(0).toUpperCase() + after.slice(1) // cuts and uppercases first letter from *after* then slices and attatches the rest to the back
+                result.push(index)
+            }else{
+             index = after
+             result.push(index)
+            }
+        }else(
+            result.push(str[i])
+        )
+    }
+    return result.join(' ');
   }
   
-  myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+  //console.log(myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped"))
+  console.log(myReplace("He is Sleeping on the couch", "Sleeping", "sitting"))
+
+
+  /*
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog
+  */
