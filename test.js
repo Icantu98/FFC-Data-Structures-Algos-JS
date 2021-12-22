6226e982c477c20065c5a6da20e90c9bd1967213
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-22 08:04:54 -0600

    .

--- test.js
+++ test.js
@@ -1,4 +1,2 @@
-arr = ([1, 3, 2], [5, 2, 1, 4], [2, 1])
-console.log(arr[2])
-
-arr1.concat(arr2).filter(item => !arr1.includes(item) || !arr2.includes(item))
+let arr1 = [1, 3, 2]
+let arr2 = [5, 2, 1, 4]
