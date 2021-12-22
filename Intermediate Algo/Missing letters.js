a0a678f3d8130dace6f7b90836a70f2ee219eed1
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-21 22:43:17 -0600

    changing over to ASCII method

---
+++ Intermediate Algo/Missing letters.js
@@ -0,0 +1,17 @@
+function fearNotLetter(str) {
+    for(let i = 0; i < str.length; i++)
+    var code = str.charCodeAt(0)
+
+
+    return code;
+}
+  
+ console.log(fearNotLetter("abce"))
+// console.log(fearNotLetter("abcdefghjklmno"))
+// console.log(fearNotLetter("stvwx"))
+// console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"))
+  /*
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.
+  */
