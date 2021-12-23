577019ecf2f2a1ac55d051b8d79f6a1c03575674
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-23 13:46:36 -0600

    done. interesting truth checker with length of arr

---
+++ Intermediate Algo/Everything be True.js
@@ -0,0 +1,25 @@
+function truthCheck(collection, pre) {
+	let count = 0 // to count up all the truthys in array of objs. Then at the end check if this matches the length of the array. If true then all are Truthy
+	for (let obj in collection){
+		if(collection[obj].hasOwnProperty(pre) && Boolean(collection[obj][pre])){
+			count ++
+		}
+	}
+	return count == collection.length
+}
+  
+  console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"))
+  console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"))
+  console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"))
+  
+  
+  /* 
+Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+In other words, you are given an array collection of objects. The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Remember, you can access object properties through either dot notation or [] notation.
+
+  */
