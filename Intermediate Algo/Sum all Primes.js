e9de6f46dacff08a51c4738623cd58513ee7c080
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-22 19:12:28 -0600

    added Sieve algo from hints. not working. stalls

--- Intermediate Algo/Sum all Primes.js
+++ Intermediate Algo/Sum all Primes.js
@@ -1,10 +1,21 @@
 function sumPrimes(num) {
-
-  return num;
+	// have to start prime list from 2
+	var sieve = []
+	var primes = []
+	for (let i =2; i <= num; i ++){
+		if(!sieve[i]){
+			// i has not been marked -- it is prime
+			primes.push(i)
+			for(let j = i <<1; j <= num; j=+i){
+				sieve[j] = true;
+			}
+		}
+	}
+  return primes;
 }
   
   console.log(sumPrimes(10))
-  console.log(sumPrimes(997))
+  //console.log(sumPrimes(997))
 
   /*
 A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
