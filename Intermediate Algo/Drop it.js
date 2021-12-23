41d464f3ba5e3ecac8350698ed8cd474799824e9
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-23 11:42:41 -0600

    done

--- Intermediate Algo/Drop it.js
+++ Intermediate Algo/Drop it.js
@@ -1,5 +1,10 @@
 function dropElements(arr, func) {
-    return arr;
+	for(let i =0; i < arr.length; i++){
+    	if (func(arr[i])){
+      		return arr.slice(i)
+    	}
+	}
+	return []
   }
   
   console.log(dropElements([1, 2, 3], function(n) {return n < 3; }))
@@ -8,7 +13,8 @@ function dropElements(arr, func) {
   console.log(dropElements([1, 2, 3, 4], function(n) {return n > 5;}))
 
 /* 
-Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated
+ element is passed through it.
 
 Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array. 
 */
