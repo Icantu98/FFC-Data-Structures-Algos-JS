d0bb9793df75cfcc5607be0c0d6b534c8262115d
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-22 23:55:12 -0600

    Done, with help from hints. learn math behind it

--- Intermediate Algo/Smallest Common Multiple.js
+++ Intermediate Algo/Smallest Common Multiple.js
@@ -1,8 +1,30 @@
 function smallestCommons(arr) {
-    return arr;
+  const [min, max] = arr.sort((a, b) => a - b) // sorts args to get range correctly 
+  const numberDivisors = max - min + 1
+    // I do NOT understand the point of this block
+  let upperBound = 1
+    for(let i = min; i <=max; i++){
+      upperBound *= i
+    }
+    // IDK how this FOR block works
+    // Test all multiples of 'max'
+    for(let multiple = max; multiple <= upperBound; multiple += max) {
+      let divisorCount = 0;
+      for(let i = min; i <= max; i++){
+        if (multiple %i === 0){
+          divisorCount += 1
+        }
+      }
+      if (divisorCount === numberDivisors){
+        return multiple
+      }
+    }
   }
   
-  smallestCommons([1,5]);
+  console.log(smallestCommons([1,5]))
+  console.log(smallestCommons([7,5]))
+  console.log(smallestCommons([2,10]))
+  console.log(smallestCommons([1,13]))
 
   /* 
 Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
