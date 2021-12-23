7fad27c8b1e8fc8a19a7e8df952e3149ea31bbc3
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-23 02:07:21 -0600

    done

---
+++ Basic Algo/Reverse a string.js
@@ -0,0 +1,9 @@
+function reverseString(str) {
+    let result = []
+    for (let i = 0; i < str.length; i++){
+        result.unshift(str[i])
+    }
+    return result.join('')
+  }
+  
+  console.log(reverseString("hello"))
