a65c494c6dc572f5a111641152cec172ce36ea9d
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-23 03:08:55 -0600

    done

---
+++ Basic Algo/Falsy Bouncer.js
@@ -0,0 +1,16 @@
+function bouncer(arr) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i]) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+
+/* 
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+
+*/
