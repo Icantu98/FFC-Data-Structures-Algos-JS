55c7df456a7e59621950b1c4a78af136e76b4202
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-22 19:24:49 -0600

    done

--- Intermediate Algo/Sum all Primes.js
+++ Intermediate Algo/Sum all Primes.js
@@ -1,21 +1,25 @@
 function sumPrimes(num) {
 	// have to start prime list from 2
+	var sum = []
 	var sieve = []
 	var primes = []
-	for (let i =2; i <= num; i ++){
+	// This Sieve for loop from https://stackoverflow.com/questions/11966520/how-to-find-prime-numbers-between-0-100
+	for (let i =2; i <= num; ++i){
 		if(!sieve[i]){
 			// i has not been marked -- it is prime
-			primes.push(i)
-			for(let j = i <<1; j <= num; j=+i){
+			primes.push(i);
+			for(let j = i <<1; j <= num; j+=i){
 				sieve[j] = true;
 			}
 		}
 	}
-  return primes;
+	//console.log(primes)
+	sum = primes.reduce((result,number) => result + number)
+  return sum;
 }
   
   console.log(sumPrimes(10))
-  //console.log(sumPrimes(997))
+  console.log(sumPrimes(997))
 
   /*
 A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
