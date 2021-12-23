e4b47cfba48acac8f18ab225004b4ade662f4b92
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-23 02:39:24 -0600

    done

--- Basic Algo/Longest word in string.js
+++ Basic Algo/Longest word in string.js
@@ -1,6 +1,12 @@
 function findLongestWordLength(str) {
     let array = str.split(' ')
-    return array
+    let longest = 0
+    for (let i = 0; i < array.length; i++){
+      if (array[i].length > longest){
+        longest = array[i].length
+      }
+    }
+    return longest
   }
   
 console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"))
