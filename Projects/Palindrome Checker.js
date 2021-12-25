d2df1f2eca767c758b10bd625df193c4e2527c4e
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-25 00:36:14 -0600

    idea

--- Projects/Palindrome Checker.js
+++ Projects/Palindrome Checker.js
@@ -1,4 +1,5 @@
 function palindrome(str) {
+//   make a .push for loop after you clean the data. then check it against the original clean compressed string. then output true/false
     return true;
   }
   
