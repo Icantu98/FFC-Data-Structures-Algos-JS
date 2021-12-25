9659c8e0515c3ad44fb607e4e9f78cdedbc6b664
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-25 12:29:45 -0600

    settting up test cases

--- Projects/Palindrome Checker.js
+++ Projects/Palindrome Checker.js
@@ -1,6 +1,13 @@
 function palindrome(str) {
+	let array1 = str.toLowerCase() 
 //   make a .push for loop after you clean the data. then check it against the original clean compressed string. then output true/false
-    return true;
+    return array1;
   }
   
-  palindrome("eye");
+  console.log(palindrome("eye"))
+  console.log(palindrome("_eye"))
+  console.log(palindrome("race car"))
+  console.log(palindrome("not a palindrome"))
+  console.log(palindrome("A man, a plan, a canal. Panama"))
+  console.log(palindrome("0_0 (: /-\ :) 0-0"))
+  console.log(palindrome("five|\_/|four"))
