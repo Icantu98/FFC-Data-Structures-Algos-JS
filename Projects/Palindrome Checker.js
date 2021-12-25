b0f8a07ab19c166ca61f7d192a173aa8a8f873b8
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-25 12:33:40 -0600

    set up reverse and final test loops

--- Projects/Palindrome Checker.js
+++ Projects/Palindrome Checker.js
@@ -1,7 +1,22 @@
 function palindrome(str) {
-	let array1 = str.toLowerCase() 
+	let array1 = str.toLowerCase() // clean data
+	let array2 = [] // for reversed string
 //   make a .push for loop after you clean the data. then check it against the original clean compressed string. then output true/false
-    return array1;
+
+
+	return array1; // for debuging
+
+	// reverse string loop
+	for (let i = 0; i < array1.length; i++){
+		array2.unshift(array1[i])
+	}
+	// Check if arr1 and arr2 are the same.
+	if (array1 == array2){
+		return true
+	}else{
+		return false
+	}
+
   }
   
   console.log(palindrome("eye"))
