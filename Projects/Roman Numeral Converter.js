adab864fbfa6ed7fdde0563eed8f861b5399cba8
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-25 19:40:09 -0600

    few test setupts

--- Projects/Roman Numeral Converter.js
+++ Projects/Roman Numeral Converter.js
@@ -1,5 +1,13 @@
 function convertToRoman(num) {
     return num;
    }
+
+
+   console.log(convertToRoman(2))
+   console.log(convertToRoman(4))
+   console.log(convertToRoman(5))
+   console.log(convertToRoman(6))
+   console.log(convertToRoman(9))
+   console.log(convertToRoman(11))
+   console.log(convertToRoman(16))
    
-   convertToRoman(36);
