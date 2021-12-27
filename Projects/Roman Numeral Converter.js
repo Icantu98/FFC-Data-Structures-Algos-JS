89bf2a6b2fadaac85f24f32966d088556fc46228
Isaac Cantu <isaac.cantu707@gmail.com>
2021-12-26 19:16:18 -0600

    .

--- Projects/Roman Numeral Converter.js
+++ Projects/Roman Numeral Converter.js
@@ -24,8 +24,8 @@ function convertToRoman(num) {
     } if(num >=1){
         remainder[6] = Math.floor(num/1)
     }
-    // return remainder;// for testing
-
+    return remainder;// for testing
+	//	YOU HAVE TO REVERSE RANKS FOR 4'S!!!
     // make a string that multiplies with array?? need to fix for below keyvalues first
     let romanNum = 'M'
     return romanNum
