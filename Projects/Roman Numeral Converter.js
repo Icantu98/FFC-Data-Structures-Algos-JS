e5df327d7958afe8be66b33cb98ab5fcd8fbb638
Isaac Cantu <isaac.cantu707@gmail.com>
2022-01-02 03:08:26 -0600

    Learned about lookup table with and Object.

--- Projects/Roman Numeral Converter.js
+++ Projects/Roman Numeral Converter.js
@@ -1,70 +1,27 @@
 function convertToRoman(num) {
-    let remainder = [0,0,0,0,0,0,0]; // sets multipliers for each rank
-    let rank = ['M','D','C','L','X','V','I'] // ranks for each Roman
-    // function to loop througb remainderIndex and fill values
-    function remainderFill(remainderIndex,romanDivisor){
-        remainder[remainderIndex] = Math.floor(num/romanDivisor)
-        num = num - (romanDivisor*remainder[remainderIndex])
+    let romanNum = ''
+    let lookup = {
+        M:1000,
+        CM:900,
+        D:500,
+        CD:400,
+        C:100,
+        XC:90,
+        L:50,
+        XL:40,
+        X:10,
+        IX:9,
+        V:5,
+        IV:4,
+        I:1
     }
-
-     // fill out the remainders
-    if(num >= 1000){
-        remainderFill(0,1000)
-    } if(num >= 500){
-        remainderFill(1,500)
-    } if(num >= 100){
-        remainderFill(2,100)
-    } if(num >= 50){
-        remainderFill(3,50)
-    } if(num >= 10){
-        remainderFill(4,10)
-    } if(num >= 5){
-        remainderFill(5,5)
-    } if(num >=1){
-        remainderFill(6,1)
-    }
-    // return remainder;// for testing
-
-	//	YOU HAVE TO REVERSE RANKS FOR 4'S!!!
-    // make a string that multiplies with array?? need to fix for below keyvalues first
-
-    // Function for string writring
-    function dec2Rome(remainderIndex,rankIndex){
-        let numIndex = []
-        switch(remainder[remainderIndex]){
-            case 4:
-                romanNum.push(rank[rankIndex])
-                romanNum.push(rank[rankIndex-1])
-                remainder[remainderIndex+1] = 0
-                break;
-            case 9: // This case will never exist cause its decimal not romannum rank
-                console.log(9)
-                break;
-            default:
-                while(remainder[remainderIndex] > 0){
-                    romanNum.push(rank[rankIndex])
-                    remainder[remainderIndex] -= 1
-                }
+    for(let i in lookup){
+        while (num >= lookup[i]){
+            romanNum += i;
+            num -= lookup[i]
         }
     }
-    let romanNum = []
-    if(remainder[0] >= 1){
-        dec2Rome(0,0)
-    }if(remainder[1] >=1){
-        dec2Rome(1,1)
-    }if(remainder[2] >=1){
-        dec2Rome(2,2)
-    }if(remainder[3] >=1){
-        dec2Rome(3,3)
-    }if(remainder[4] >=1){
-        dec2Rome(4,4)
-    }if(remainder[5] >=1){
-        dec2Rome(5,5)
-    }if(remainder[6] >=1){
-        dec2Rome(6,6)
-    }
-
-    return romanNum.join('')
+    return romanNum
 }
 
 
